797f18f7d086e05160cbe439d643a357fcfe9f22
gandib <55914763+gandib@users.noreply.github.com>
2025-03-05 18:17:08 +0600

    Project initialize

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="bg-[#242424] min-w-[1440px] min-h-[900px] rounded-[32px] p-4 gap-3 flex justify-between">
+      {/* sidebar */}
+      <div className="w-[280px] p-3 rounded-[24px] h-[868px]">56456</div>
+
+      {/* content */}
+      <div className="bg-[#2B2B2B] w-[1116px] rounded-[16px] pt-10 px-5 pb-5 gap-20 h-[868px]">
+        fgf
+      </div>
+    </div>
+  );
+}
+
+export default App;
